54d97bf613151b27b2bbdd6231a73ca089365932
Kirito <kirito.machida@gmail.com>
2025-04-26 21:56:25 -0400

    finished the trading feature, added the details to marketplace

--- swap_house/urls.py
+++ swap_house/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('post/', views.post_trade_view, name='post_trade'),
     path('trades/', views.trade_list_view,  name='trade_list'),
     path('trades/<int:trade_id>/offer/', views.offer_trade_view, name='offer_trade'),
+    path('offers/received/', views.received_offers_view, name='received_offers'),
     path('trades/<int:trade_id>/', views.trade_detail_view, name='trade_detail'),
     path('trades/<int:trade_id>/offer/<int:offer_id>/accept/', views.accept_offer_view, name='accept_offer'),
     path('trades/<int:trade_id>/offer/<int:offer_id>/reject/', views.reject_offer_view, name='reject_offer'),
